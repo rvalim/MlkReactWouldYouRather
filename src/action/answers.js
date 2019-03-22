1fa6d6cd4de3dd7cae9f50bf99a65b5086293615
unknown <ricardo.valim@gmail.com>
2019-03-22 14:42:48 +0000

    bug fixed when choosing option

--- src/action/answers.js
+++ src/action/answers.js
@@ -2,28 +2,26 @@ import * as data from '../utils/data'
 
 export const SEL_OPTION = 'SEL_OPTION'
 
-function _chooseOne(authedUser, qId, answer) {
+function _chooseOne(authedUser, qid, answer) {
     return {
         type: SEL_OPTION,
         authedUser, 
-        qId,
+        qid,
         answer
     }
 }
 
-export function chooseOne(qId, answer) {
-    console.log('chooseOne', qId, answer)
+export function chooseOne(qid, answer) {
     return (dispatch, getState) => {
         const { authedUser, questions } = getState()
-console.log('chooseOne', qId, answer, questions[qId])
-        const qOne = questions[qId].optionOne.text
-        const qTwo = questions[qId].optionTwo.text
+        const qOne = questions[qid].optionOne.text
+        const qTwo = questions[qid].optionTwo.text
         const res = qOne === answer ? 'optionOne' : (qTwo === answer? 'optionTwo' : undefined)
 
         if (!res) throw new Error('Answer not in the list of options')
 
-        return data._saveQuestionAnswer({ authedUser, qId, res })
-            .then(question => dispatch(_chooseOne(authedUser, qId, res )))
+        return data._saveQuestionAnswer({ authedUser, qid, answer: res })
+            .then(question => dispatch(_chooseOne(authedUser, qid, res )))
         //.then(() => dispatch(hideLoading()))
     }
 }
